73936d97bed5da5fc3c044d736b00af573956392
agathasta <kalidonia@hotmail.com>
2021-01-03 18:10:07 +0100

    Start refactoring Matching Brackets

--- matching-brackets/matching_brackets.rb
+++ matching-brackets/matching_brackets.rb
@@ -4,11 +4,23 @@ class Brackets
   BRACKETS = { ']' => '[', '}' => '{', ')' => '(' }
 
   def self.paired?(text)
-    text.each_char.with_object(open_brackets = []) do |bracket, open_brackets|
+    new(text).paired?
+  end
+
+  attr_reader :brackets
+  def initialize(text)
+    @brackets = text.scan(/[\[\]\{\}\(\)]*/).join.chars
+  end
+
+  def paired?
+    compare_brackets.empty?
+  end
+
+  def compare_brackets
+    brackets.each_with_object(open_brackets = []) do |bracket, open_brackets|
       open_brackets << bracket if BRACKETS.values.include?(bracket)
       mirror_bracket = open_brackets.pop if BRACKETS.keys.include?(bracket)
-      return false unless mirror_bracket == BRACKETS[bracket]
+      return 'not_balanced' unless mirror_bracket == BRACKETS[bracket]
     end
-    open_brackets.empty?
   end
 end
