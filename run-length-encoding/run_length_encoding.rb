85b2a9ba4331f00bdb25954d3b9f9503b3be6b12
agathasta <kalidonia@hotmail.com>
2021-01-13 09:18:15 +0100

    Refractor decoding with Regex

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,20 +1,10 @@
 # Implement run-length encoding and decoding.
 class RunLengthEncoding
-  def self.encode(text)#, counter = 1, code = [])
-    text.gsub(/(.)\1+/) {|s| "#{s.length}#{s[0]}"}
+  def self.encode(plaintext)
+    plaintext.gsub(/(.)\1+/) { |group| "#{group.length}#{group[0]}" }
   end
 
-  def self.decode(code, multiplier = 1, text = [])
-    code.scan(/\d+|\D+/).each do |key|
-      if /^\d+$/.match(key)
-        multiplier = key.to_i
-      else
-        key.each_char do |char|
-          text << char * multiplier
-          multiplier = 1
-        end
-      end
-    end
-    text.join
+  def self.decode(code)
+    code.gsub(/(\d+)(\D)/) { Regexp.last_match(2) * Regexp.last_match(1).to_i }
   end
 end
